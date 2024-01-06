a81b899080fc827182e03dd56735176d848739fb
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-01-06 13:17:04 +0200

    Main file basing and integration
    Two things:
    1. Adding the main file to the project
    2. Creating the skeleton of the main file

---
+++ Personal Work For C++/main.cpp
@@ -0,0 +1,14 @@
+#define NULL 0
+// Constraints and Dependencies //
+
+// Class headers //
+
+int main() { return NULL; }
+
+/*
+* TODO:
+* 1. Add the inner function headers
+* 2. Add the inner function scope & Variables
+* 3. Add the inner function logic
+
+*/
