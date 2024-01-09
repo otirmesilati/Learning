9ab225438a4c4c039aeffa4b21532109f49672cc
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-01-09 10:31:32 +0200

    Testing

--- Personal Work For Ruby/Work_Shop_1.rb
+++ Personal Work For Ruby/Work_Shop_1.rb
@@ -1,1 +1,10 @@
 puts "Coming soon~"
+
+def find_missing(sequence)
+  consecutive     = sequence.each_cons(2)
+  differences     = consecutive.map { |a,b| b - a }
+  sequence        = differences.max_by { |n| differences.count(n) }
+  missing_between = consecutive.find { |a,b| (b - a) != sequence }
+  missing_between.first + sequence
+
+end
