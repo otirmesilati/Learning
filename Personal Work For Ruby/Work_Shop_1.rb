4dc14fd1510a4d682af2121bf83101e542ee58b3
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-01-09 17:19:24 +0200

    Testing

--- Personal Work For Ruby/Work_Shop_1.rb
+++ Personal Work For Ruby/Work_Shop_1.rb
@@ -8,3 +8,15 @@ def find_missing(sequence)
   missing_between.first + sequence
 
 end
+
+def get_numbers_stack(list)
+  stack  = [[0, []]]
+  output = []
+  until stack.empty?
+    index, taken = stack.pop
+    next output << taken if index == list.size
+    stack.unshift [index + 1, taken]
+    stack.unshift [index + 1, taken + [list[index]]]
+  end
+  output
+end
