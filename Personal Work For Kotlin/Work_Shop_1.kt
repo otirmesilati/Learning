fced396b3e0a04a7dbe4701664b2b8cf39dbf830
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-01-09 08:45:55 +0200

    Build 1.2
    Comment Format:
    1. Added subtraction function annotation
    
    Inner implementation:
    1. Added subtraction function Scope + Logic

--- Personal Work For Kotlin/Work_Shop_1.kt
+++ Personal Work For Kotlin/Work_Shop_1.kt
@@ -8,9 +8,13 @@
 
 Function Specifications:
 
-1. add_Two_Numbers: summation of 2 numbers function
-   IN:   two integers
-   OUT:  integer
+1. addTwoNumbers: summation of 2 numbers function
+   IN:   2 integers that will be summed
+   OUT:  int summation
+
+2. subtractTwoNumbers: subtraction of 2 numbers function
+   IN:   2 integers that will be summed
+   OUT:  int summation
 
 ->> Classes: ?
 
@@ -20,4 +24,6 @@ Function Specifications:
 
 // Class headers
 
-fun addTwoNumbers(firstNumber: Int, secondNumber: Int) = firstNumber + secondNumber;
+fun addTwoNumbers(summand: Int, addend: Int) = summand + addend
+
+fun subtractTwoNumbers(minuend: Int, subtrahend: Int) = minuend - subtrahend
