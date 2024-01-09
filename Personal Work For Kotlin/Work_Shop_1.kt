b398224b1d2d498b0435970af8697967ae1e76d1
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-01-09 10:13:17 +0200

    Build 1.4
    Comment Format:
    1. Added division function annotation
    
    Inner implementation:
    1. Added division function Scope + Logic

--- Personal Work For Kotlin/Work_Shop_1.kt
+++ Personal Work For Kotlin/Work_Shop_1.kt
@@ -20,6 +20,10 @@ Function Specifications:
    IN:   2 integers that will be multiplied
    OUT:  int product
 
+3. divideTwoNumbers: division of 2 numbers function
+   IN:   2 integers that will be divided
+   OUT:  int product
+
 
 ->> Classes: ?
 
@@ -34,3 +38,5 @@ fun addTwoNumbers(summand: Int, addend: Int) = summand + addend
 fun subtractTwoNumbers(minuend: Int, subtrahend: Int) = minuend - subtrahend
 
 fun multiplyTwoNumbers(multiplier: Int, multiplicand: Int) = multiplier * multiplicand
+
+fun divideTwoNumbers(dividend: Int, divisor: Int) = dividend / divisor
