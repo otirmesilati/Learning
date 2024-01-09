ac50814599ef22b073d53e64106ef4e8b349300d
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-01-09 17:24:16 +0200

    Testing

--- Personal Work For Dart/Work_Shop_1.dart
+++ Personal Work For Dart/Work_Shop_1.dart
@@ -16,4 +16,19 @@ void main() {
   // const variables are compile-time constants
   const double d = 44.00;
   print(d);
+
+  // declare an integer
+  int num1 = 2;
+
+  // declare a double value
+  double num2 = 1.5;
+
+  // print the values
+  print(num1);
+  print(num2);
+  var a1 = num.parse("1");
+  var b1 = num.parse("2.34");
+
+  var c1 = a1+b1;
+  print("Product = ${c1}");
 }
