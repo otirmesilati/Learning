9764284d683e0ab5ad17a2fd98661eaad331b97c
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-01-09 17:17:58 +0200

    Testing

---
+++ Personal Work For C#/Work_Shop_1.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Personal_Projects
+{
+    class Personal_Works_1
+    {
+        Personal_Works_1() { }
+        public void TBD() { Console.WriteLine("Coming soon~"); }
+    }
+}
