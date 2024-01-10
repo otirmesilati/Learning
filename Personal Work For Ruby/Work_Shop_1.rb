17757c98439b3def044fd0f62c36a038a96158de
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-01-10 20:42:07 +0200

    Build 1.2
    Comment Format:
    1. Adding the subtraction function in the annotation

--- Personal Work For Ruby/Work_Shop_1.rb
+++ Personal Work For Ruby/Work_Shop_1.rb
@@ -8,9 +8,13 @@
 
 Function Specifications:
 
-1. AddTwoNumbers: Two numbers adding function
+1. addTwoNumbers: Two numbers adding function
    IN:   2 integers for summand and addend
    OUT:  integer for summation of the numbers
+
+2. subtractTwoNumbers: Two numbers subtracting function
+   IN:   2 integers for minuend and subtrahend
+   OUT:  integer for difference of the numbers
 
 ->> Classes: ?
 
@@ -22,4 +26,33 @@ Function Specifications:
 
 # Class headers
 
-# Function headers 
+# Function headers
+=begin
+#   &&&&&&&&&&&&&&&&&&&&&&&&&&&&&   Workshop ? : ?  &&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+#   &&&&&&&&&&&&&&&&&&&&&&&&&&&&&   LANGUAGE: Ruby1        &&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+#   &&&&&&&&&&&&&&&&&&&&&&&&&&&&&   Date: 10/01/2024        &&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+#   &&&&&&&&&&&&&&&&&&&&&&&&&&&&&   Author: Otir Mesilati   &&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+"""
+-> Functions: ?
+
+Function Specifications:
+
+1. addTwoNumbers: Two numbers adding function
+   IN:   2 integers for summand and addend
+   OUT:  integer for summation of the numbers
+
+2. subtractTwoNumbers: Two numbers subtracting function
+   IN:   2 integers for minuend and subtrahend
+   OUT:  integer for difference of the numbers
+
+->> Classes: ?
+
+"""
+
+=end
+
+# Constraints & Dependencies: Non.
+
+# Class headers
+
+# Function headers
