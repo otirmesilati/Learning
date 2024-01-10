f19edeb407c8d504d950389052767874ba3ff0b6
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-01-10 19:16:05 +0200

    Build 1.0
    Comment format:
    1. Adding the skeleton of the file's annotation

--- Personal Work For Ruby/Work_Shop_1.rb
+++ Personal Work For Ruby/Work_Shop_1.rb
@@ -1,42 +1,25 @@
-puts "Coming soon~"
+#   &&&&&&&&&&&&&&&&&&&&&&&&&&&&&   Workshop ? : ?  &&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+#   &&&&&&&&&&&&&&&&&&&&&&&&&&&&&   LANGUAGE: Python        &&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+#   &&&&&&&&&&&&&&&&&&&&&&&&&&&&&   Date: ?        &&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+#   &&&&&&&&&&&&&&&&&&&&&&&&&&&&&   Author: Otir Mesilati   &&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+"""
+-> Functions: ?
 
-def find_missing(sequence)
-  consecutive     = sequence.each_cons(2)
-  differences     = consecutive.map { |a,b| b - a }
-  sequence        = differences.max_by { |n| differences.count(n) }
-  missing_between = consecutive.find { |a,b| (b - a) != sequence }
-  missing_between.first + sequence
+Function Specifications:
 
-end
+1. ?: ?
+   IN:   ?
+   OUT:  ?
 
-def get_numbers_stack(list)
-  stack  = [[0, []]]
-  output = []
-  until stack.empty?
-    index, taken = stack.pop
-    next output << taken if index == list.size
-    stack.unshift [index + 1, taken]
-    stack.unshift [index + 1, taken + [list[index]]]
-  end
-  output
-end
+->> Classes: ?
 
-def longest_repetition(string)
-  max = string
-          .chars
-          .chunk(&:itself)
-          .map(&:last)
-          .max_by(&:size)
-  max ? [max[0], max.size] : ["", 0]
-end
+"""
 
-def reverse_alternate(string)
-  string.gsub(/[^\s]+/).with_index { |w, idx| idx.even? ? w : w.reverse }
-end
 
-def clean_string(str)
-  str
-    .chars
-    .each_with_object([]) { |ch, obj| ch == "#" ? obj.pop : obj << ch }
-    .join
-end
+<<Control-Flow Skeleton>>
+
+# Constraints & Dependencies: Non.
+
+# Class headers
+
+# Function headers 
