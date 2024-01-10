906f1043f54b4a91845342a55e70ab4dbad2a462
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-01-10 22:37:33 +0200

    Build 1.3
    Comment Format:
    1. Adding the multiplication function in the annotation

--- Personal Work For Ruby/Work_Shop_1.rb
+++ Personal Work For Ruby/Work_Shop_1.rb
@@ -16,34 +16,9 @@ Function Specifications:
    IN:   2 integers for minuend and subtrahend
    OUT:  integer for difference of the numbers
 
-->> Classes: ?
-
-"""
-
-=end
-
-# Constraints & Dependencies: Non.
-
-# Class headers
-
-# Function headers
-=begin
-#   &&&&&&&&&&&&&&&&&&&&&&&&&&&&&   Workshop ? : ?  &&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-#   &&&&&&&&&&&&&&&&&&&&&&&&&&&&&   LANGUAGE: Ruby1        &&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-#   &&&&&&&&&&&&&&&&&&&&&&&&&&&&&   Date: 10/01/2024        &&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-#   &&&&&&&&&&&&&&&&&&&&&&&&&&&&&   Author: Otir Mesilati   &&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-"""
--> Functions: ?
-
-Function Specifications:
-
-1. addTwoNumbers: Two numbers adding function
-   IN:   2 integers for summand and addend
-   OUT:  integer for summation of the numbers
-
-2. subtractTwoNumbers: Two numbers subtracting function
-   IN:   2 integers for minuend and subtrahend
-   OUT:  integer for difference of the numbers
+3. multiplyTwoNumbers: Two numbers multiplying function
+   IN:   2 integers for multiplier and multiplicand
+   OUT:  integer for product of the numbers
 
 ->> Classes: ?
 
