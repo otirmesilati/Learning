f8578d2f318e916bee99bc046130674100ab8ffe
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-02-13 10:22:06 +0200

    Creating 3'rd C++ Workshop
    Title: Playing around

---
+++ Personal Work For C++/Work_Shop_3.cpp
@@ -0,0 +1,71 @@
+/*
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX   WORKSHOP 3 : Playing around with the language   XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX   LANGUAGE: C++                                   XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX   Date: 13/02/2024                                XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX   Author: Otir Mesilati                           XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+->> Constraints & dependencies:
+
+Dependencies: (1)
+1. inputting and outputting to the console.
+
+Constraints:
+1. C++ Standard library:
+1.1. Namespace std:
+1.1.1. IOStream Stream section
+
+Function Specifications: (1)
+
+1. first_Worsk_Shop_Main: the main function of this file
+   IN: Non.
+   OUT: Non.
+
+1. add: A function for adding two integers
+   IN(2): Summand and Addend
+   OUT(1): Summation
+
+1. add: A function for adding two integers
+   IN(2): Summand and Addend
+   OUT(1): Summation
+
+1. add: A function for adding two integers
+   IN(2): Summand and Addend
+   OUT(1): Summation
+
+1. add: A function for adding two integers
+   IN(2): Summand and Addend
+   OUT(1): Summation
+
+Method Specifications: (1)
+
+2. Simple_String: C'tor
+   IN:  char Array
+   OUT:  Simple_String Instance
+
+Class Specifications: 1 In total
+
+Design TODOs:
+1. Use a lot of overloading
+2. Basic arithmetic functions
+3. develop from there to more functions
+4.
+
+*/
+
+#pragma once
+#include <iostream>
+using namespace std;
+
+int add(int, int) {}
+int add(int, int, int){}
+float add(float, float) {}
+float add(float, float, float) {}
+
+// definitions go here //
+
+int first_Worsk_Shop_Main() { return 0; }
+
+/* Implementation TODOs:
+1. Add macros
+2.
+*/
