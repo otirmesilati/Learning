93bfcda743bf9e2c4b4065ff2abd46f46f58aa78
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-04-27 06:54:24 +0300

    tweeeeaking :D

---
+++ Personal Work For Kotlin/Work_Shop_2.kt
@@ -0,0 +1,15 @@
+class FirstClass
+{
+    fun firstFunction() = 1
+}
+
+class FirstClass
+{
+    fun firstFunction() = 1
+
+    fun secondFunction(x : Int) {
+
+        println("Input passed: $x")
+
+    }
+}
