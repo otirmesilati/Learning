d025edec5e102e30ef3aa3ea274bad906174bb41
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-04-27 12:22:09 +0300

    some more kotlin tweaks

--- Personal Work For Kotlin/Work_Shop_2.kt
+++ Personal Work For Kotlin/Work_Shop_2.kt
@@ -1,15 +1,15 @@
 class FirstClass
 {
     fun firstFunction() = 1
-}
 
-class FirstClass
-{
-    fun firstFunction() = 1
-
-    fun secondFunction(x : Int) {
+    fun secondFunction(x : Int) : Unit {
 
         println("Input passed: $x")
 
+    }
+
+    fun thirdFunction(firstVal : Int, secondVal : Int) : Int {
+
+        return firstVal + secondVal
     }
 }
