204868f94d6eb75a79029b61c3f4e06bc7d4fcb6
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-05-14 20:06:31 +0300

    Build 0.2
    -> Design: Non.
    Non.
    
    -> Comment Formatting:
    Header, constraints and dependencies for  Ilocatable.kt file
    
    -> Implementation:
    Non.

--- Personal Work For Kotlin/ZooInKotlin/mobility/Ilocatable.kt
+++ Personal Work For Kotlin/ZooInKotlin/mobility/Ilocatable.kt
@@ -1,6 +1,29 @@
-interface Ilocatable
-{
-    public fun setLocation(): Unit 
-    public fun getLocation(): Unit 
-}
+/*  &&&&&&&&&&&&&&&&&&&&&&&&&&&&&   Workshop ? : ?  &&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+    &&&&&&&&&&&&&&&&&&&&&&&&&&&&&   LANGUAGE: Kotlin        &&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+    &&&&&&&&&&&&&&&&&&&&&&&&&&&&&   Date: ?        &&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+    &&&&&&&&&&&&&&&&&&&&&&&&&&&&&   Author: Otir Mesilati   &&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
+-> Functions: ?
+
+Function Specifications:
+
+1. ?: ?
+   IN:   ?
+   OUT:  ?
+
+->> Classes: ?
+
+*/
+
+package mobility
+
+interface Ilocatable {
+
+    //TODO: public Point getLocation()
+
+    //TODO: boolean setLocation (Point locationToSet)
+
+    public fun setLocation(): Unit
+    public fun getLocation(): Unit
+
+}
