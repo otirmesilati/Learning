f9ca33f6ea77f82725cd9febd86d747474b5e57b
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-06-07 18:53:51 +0300

    Javaing

--- Personal Work For Java/Work_Shop_2.java
+++ Personal Work For Java/Work_Shop_2.java
@@ -1,3 +1,15 @@
 public class Work_Shop_2 {
+
+        public static void main(String[] args) {
+            System.out.println("5");
+            System.gc();
+            System.out.println(6);
+    
+            // My 1 line comment //
+            /* My 2 line comment */
+            /** My log */
+    
+            
+        }
     
 }
