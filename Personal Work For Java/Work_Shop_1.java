3877c6cffa1f6b8b68a835d6f3e8992c568182db
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-06-07 16:13:23 +0300

    Javaing

---
+++ Personal Work For Java/Work_Shop_1.java
@@ -0,0 +1,30 @@
+// import java.awt.Point;
+// import java.util.Random;
+
+public class Work_Shop_1 {
+	
+	// private static class innerObject 
+	// {
+		// private int myField = 5;
+		// public void setField(int x) {myField = x;}
+		// public int returnFive() {return 5;}
+    // 	}
+	
+	public interface myInter
+	{
+		 void myMethod();
+	}
+	
+	public static void main(String[] args) 
+	{
+		// final innerObject oe; 
+		int a = 1;
+		int b = 2;
+		// oe = new innerObject();
+		System.out.println("Hi");
+	}
+	
+	
+	
+
+}
