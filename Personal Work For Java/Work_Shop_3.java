8b534d72f9e049447f0957cd05c13e7b39453ce7
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-06-08 08:21:33 +0300

    Javaing

--- Personal Work For Java/Work_Shop_3.java
+++ Personal Work For Java/Work_Shop_3.java
@@ -1,4 +1,47 @@
 public class Work_Shop_3 
 {
-    
+    /**
+ * A class for testing the DP of singleton
+ * 
+ * @author otirm_2hwnj4i
+ *
+ */
+
+ public class UnClassicSingleton {
+
+	/**
+	 * Standard empty implementation of singleton class 
+	 * 
+	 * @author otirm_2hwnj4i
+	 *
+	 */
+	private static class theSingleton
+	{
+		/**
+		 * Private C'tor (Encapsulation: thus IMMUTABLE)
+		 * 
+		 */
+		private theSingleton() {}
+		
+		private static final theSingleton wrappedInstance = new theSingleton();
+		
+		/**
+		 * Get instance method for the singleton instance 
+		 * 
+		 * @return the reference of the instance
+		 * 
+		 */
+		public static theSingleton getInstance() 
+		{
+			return wrappedInstance;	
+		} 
+		
+		public static void printSingleton() 
+		{
+			if(wrappedInstance == null) System.out.println("No singleton yet!");
+			else System.out.println("The singleton was created :)");
+		}
+	}
+}
+
 }
