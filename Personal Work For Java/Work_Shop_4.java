0fbf76cc520cbf954453ffb1cf532550846d5296
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-06-09 07:19:00 +0300

    More coffee

--- Personal Work For Java/Work_Shop_4.java
+++ Personal Work For Java/Work_Shop_4.java
@@ -2,5 +2,11 @@ import java.lang.Cloneable;
 
 public class Work_Shop_4 {
     
-    public static class cloneableClass implements Cloneable{}
+    public static class cloneableClass implements Cloneable
+    {
+        protected Object clone() throws CloneNotSupportedException
+        {
+            return super.clone();
+        }
+    }
 }
