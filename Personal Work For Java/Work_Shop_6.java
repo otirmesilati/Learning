13a30720140b045a304a7a8cbc96e33ae77f8d23
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-06-30 14:18:37 +0300

    Coffee!

--- Personal Work For Java/Work_Shop_6.java
+++ Personal Work For Java/Work_Shop_6.java
@@ -8,14 +8,16 @@ public class Work_Shop_6 {
         {
             public int getProductNumber();
             public static final String rootProductString = "BaseProduct";
-            // Idea is - every product has a number 
-            // And the string is going to be mutated along the control flow //
         }
 
         private static interface ProductTypeB extends BaseProduct
         {
-            public String getTypeBModel();
-            // More interfaces for more factories! 
+            public String getTypeBModel(); 
+        }
+
+        private static interface ProductTypeC extends BaseProduct
+        {
+            public String getTypeCModel();
         }
      }
 }
