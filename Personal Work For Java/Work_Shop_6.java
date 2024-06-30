701241db9a13760ffd0025b35ccf06b502071990
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-06-30 06:50:43 +0300

    Coffee

---
+++ Personal Work For Java/Work_Shop_6.java
@@ -0,0 +1,12 @@
+public class Work_Shop_6 {
+    
+     // import FctorymethodTesting.*;
+
+     public class ClassicAbstractFactory
+     {
+        private interface BaseProduct
+        {
+            // headers headers
+        }
+     }
+}
