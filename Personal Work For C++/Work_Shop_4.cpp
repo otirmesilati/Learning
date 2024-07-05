f18e8b00a7af501b255ce3218053f54d1fb153bf
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-07-05 07:48:08 +0300

    cpping

--- Personal Work For C++/Work_Shop_4.cpp
+++ Personal Work For C++/Work_Shop_4.cpp
@@ -1,11 +1,15 @@
-#pragma once // starting a program
-#include <iostream> // IO
+#pragma once // contrain the header only once //
 using namespace std; // stding!
 
 class My_Class 
 {
-    public: // Rich API
-    private: // Encapsulation
+    public: // This whole class will be used as a container for more classes //
+    class My_Inline_Point // all definitions will be inside the header //
+    {
+        private: // Encapsulations
+        int my_x;
+        int my_y;
+    };
 };
 
 
