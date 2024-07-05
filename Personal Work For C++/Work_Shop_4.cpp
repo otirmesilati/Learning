53288738f3501e4d2439dddfd0d1ee90dca86ae8
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-07-05 07:54:36 +0300

    cpping

--- Personal Work For C++/Work_Shop_4.cpp
+++ Personal Work For C++/Work_Shop_4.cpp
@@ -6,6 +6,11 @@ class My_Class
     public: // This whole class will be used as a container for more classes //
     class My_Inline_Point // all definitions will be inside the header //
     {
+        public:
+        // rich API//
+        void set_my_x(int x_to_set){} // starting with void as default logic //
+        void set_my_y(int y_to_set){} // starting with void as default logic //
+        
         private: // Encapsulations
         int my_x;
         int my_y;
