ccfe3f3f68a2c752e8e3ab8a201e5481b6db5598
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-07-05 08:08:34 +0300

    cpping

--- Personal Work For C++/Work_Shop_4.cpp
+++ Personal Work For C++/Work_Shop_4.cpp
@@ -10,6 +10,10 @@ class My_Class
         bool set_my_x(int x_to_set){ my_x = x_to_set; return true;} // starting with void as default logic //
         bool set_my_y(int y_to_set){ my_y = y_to_set; return true;} // starting with void as default logic //
         
+        bool is_x_Bigger_then_5() {return true;} // default logic
+        int factor_y() {} //default logic
+        float rational_number() {return 0.0f;} // defualt logic
+
         int get_my_x(){return my_x;}
         int get_my_y(){return my_y;}
 
