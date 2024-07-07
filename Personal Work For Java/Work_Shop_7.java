6e35bf9ffc993d1ca65a02f789243b5c7d691fb9
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-07-07 09:54:34 +0300

    Coffeeing

--- Personal Work For Java/Work_Shop_7.java
+++ Personal Work For Java/Work_Shop_7.java
@@ -15,5 +15,8 @@ public class Work_Shop_7
 
 
         // let's create the first component //
-    }    private static abstract class FirstComponent implements Work_Shop_7.ClassicBuilder.Component{ public String toString(){return "fisrt component";}}
+    }    
+    
+    private static abstract class FirstComponent implements Work_Shop_7.ClassicBuilder.Component { public String toString() { return "fisrt component";}}
+    private static abstract class SecondComponent implements Work_Shop_7.ClassicBuilder.Component { public String toString() { return "second component";}}
 }
