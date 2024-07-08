1346fbb59ca2bae260bcf4f70f261bd64e513ffe
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-07-08 15:04:26 +0300

    Cpping

--- Personal Work For C++/Work_Shop_5.cpp
+++ Personal Work For C++/Work_Shop_5.cpp
@@ -3,6 +3,13 @@ class My_Class
 
     class Two_Digits
     {
+        public:
+
+        bool set_first() {return true;} // finish //
+        // the implementation and do the second aswell //
+
+        private:
+
         char first, second;
     };
 
