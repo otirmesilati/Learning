3284adacfc256e16ba0023b792d04bf8274361f7
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-07-08 15:37:12 +0300

    Cpping

--- Personal Work For C++/Work_Shop_5.cpp
+++ Personal Work For C++/Work_Shop_5.cpp
@@ -1,4 +1,4 @@
-class My_Class
+class My_First_Class
 {
 
     class Two_Digits
@@ -8,10 +8,13 @@ class My_Class
         bool set_first(char input_to_set) {first = input_to_set; return true;}
         bool set_second(char input_to_set) {second = input_to_set; return true;}
 
+        void show(); // we will explicit inline this //
+
         private:
 
         char first, second;
     };
 
-
 };
+
+inline void My_Class::Two_Digits::show() {}
