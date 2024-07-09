b2cf2f1a3d78659afbcfd10ac2f40f86a96e11b7
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-07-09 14:40:04 +0300

    JAV

--- Personal Work For Java/Work_Shop_7.java
+++ Personal Work For Java/Work_Shop_7.java
@@ -13,22 +13,18 @@ public class Work_Shop_7
         
         }
 
+        private static abstract class FirstComponent implements Work_Shop_7.ClassicBuilder.Component { public String toString() { return "fisrt component";}}
+        private static abstract class SecondComponent implements Work_Shop_7.ClassicBuilder.Component { public String toString() { return "second component";}}
+        private static abstract class ThirdComponent implements Work_Shop_7.ClassicBuilder.Component { public String toString() { return "third component";}}
 
-        // let's create the first component //
-    }    
-    
-    private static abstract class FirstComponent implements Work_Shop_7.ClassicBuilder.Component { public String toString() { return "fisrt component";}}
-    private static abstract class SecondComponent implements Work_Shop_7.ClassicBuilder.Component { public String toString() { return "second component";}}
-    private static abstract class ThirdComponent implements Work_Shop_7.ClassicBuilder.Component { public String toString() { return "third component";}}
-
-    private static class FirstComponentFirstConcrete extends FirstComponent
-    {
+        private static class FirstComponentFirstConcrete extends FirstComponent{}
+                
         public String toString() {return (super.toString()).concat(firstImplementationAddon);}
 
         public void ComponentsFirstMethod() {System.out.println("First Component's, model 1, first operation.");}
         public void ComponentsSecondMethod() {System.out.println("Second Component's, model 1, second operation.");}
         public void ComponentsThirdMethod() {System.out.println("Third Component's, model 1, third operation.");}
-        
+
         private String firstImplementationAddon = ", Model 1";
     }
 }
