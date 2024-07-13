34c54f4e585dcabe4d170af93b83534735b6888f
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-07-13 09:18:15 +0300

    .NET

--- Personal Work For Visual Basic/Work_Shop_1.vb
+++ Personal Work For Visual Basic/Work_Shop_1.vb
@@ -1,5 +1,9 @@
 Public Class Form1
     Private Sub brnStart_Click(sender As Object, e As EventArgs) Handles BtnStart.brnStart_Click
+    
+    MsgBox("Hello World")
+    MsgBox("How Are you?")
+    MsgBox("I hope you are well")
 
     End Sub
 End Class
