d96c5e54a013e84c86f1d155067516549a6e235d
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-07-14 05:53:01 +0300

    More Snaking

--- Personal Work For Python/Work_Shop_4.py
+++ Personal Work For Python/Work_Shop_4.py
@@ -36,5 +36,6 @@ def math_playground():
     print(termial(3))
     print(multiplication(2,3))
     print(multiplication_plus(2,3,4))
+    print(squaring(4))
     
     # Going after each of the functions defined above    
