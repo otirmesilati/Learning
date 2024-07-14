c486000c2df5e4f2bb1056ed4a5d25b71983f6a6
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-07-14 15:46:20 +0300

    Shelling

--- Personal Work For PowerShell/Work_Shop_3.ps1
+++ Personal Work For PowerShell/Work_Shop_3.ps1
@@ -1,10 +1,11 @@
+# Printing some of the host's local data 
+
 Write-Output $Env:USERNAME
 Write-Output $Env:COMPUTERNAME
 Write-Output $Env:HOMEDRIVE
 Write-Output $Env:HOMEPATH
 Write-Output $Env:WINDIR
-
-echo %systemdrive%
+Write-Output $Env:SYSTEMDRIVE
 
 arp -a
 
