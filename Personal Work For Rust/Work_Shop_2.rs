07d9a5888545bf63efe1304d177b4dbb6a1b6d2e
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-07-16 07:19:57 +0300

    Rust patch
    String formatting

--- Personal Work For Rust/Work_Shop_2.rs
+++ Personal Work For Rust/Work_Shop_2.rs
@@ -1,6 +1,17 @@
 // Commenting
+/* 
+More
+Comments
+ */
+
 
 fn main()
 {
     println!("Hello World!")
+
+    // String formatting - incremental //
+    print!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob")
+    
+    // String formatting - no order significance //
+    println!("{subject} {verb} {object}") // finish this //    
 }
