6071d5f2e624403a817bd4b6b318d7205c525442
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-07-16 11:56:58 +0300

    Batching

--- Personal Work For Batch/Work_Shop_8.bat
+++ Personal Work For Batch/Work_Shop_8.bat
@@ -11,9 +11,13 @@ start eclipse.exe
 start pycharm64.exe
 start code.exe
 
+:: about the start flags/options:
 :: probably not going to input arguments for 
 :: priority (self manage scheduler) and window size
 :: diffrentiation (opening last conf size probably best) 
+
+::  Environment variables
+echo %username%
 
 systeminfo
 tasklist
