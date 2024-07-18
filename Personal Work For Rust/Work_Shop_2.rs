483184cab3be6aaf318903e9a950d74b124f3358
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-07-18 11:20:10 +0300

    Rust
    More printing options

--- Personal Work For Rust/Work_Shop_2.rs
+++ Personal Work For Rust/Work_Shop_2.rs
@@ -7,11 +7,16 @@ Comments
 
 fn main()
 {
+    // Note the printing is MACRO based, :o/b/x for diffrent radixes //
     println!("Hello World!")
 
-    // String formatting - incremental //
+    // incremental //
     print!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob")
     
-    // String formatting - no order significance //
-    println!("{subject} {verb} {object}") // finish this //    
+    // no order significance //
+    println!("{subject} {verb} {object}", object = "the lazy dog", subject = "the quick brown fox", verb = "jumps over")
+
+    // add here:
+    // left/right justify
+    // adding zeros 
 }
