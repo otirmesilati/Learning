fca9a02d1fcb082b2de089f44a7d2e8029ee35cb
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-07-20 17:12:18 +0300

    Haskell Patch
    Indendation is important in HS!

--- Personal Work For Haskell/Work_Shop_1.hs
+++ Personal Work For Haskell/Work_Shop_1.hs
@@ -8,6 +8,11 @@ putStrLn "well done"
 double x = x + x
 double 3
 
-{
-    
-}
+:{
+    map f list = 
+        case list of
+            [] -> []
+            x:xs -> f x : map f xs
+:}
+map (+1) [1,2,3]
+
