8275d3f4b2a68b4f901739f0f14e231c20f2314a
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-07-22 16:39:32 +0300

    Elixir Patch
    printing and pattern matching

--- Personal Work For Elixir/Work_Shop_2.ex
+++ Personal Work For Elixir/Work_Shop_2.ex
@@ -22,6 +22,16 @@ strings
 [1 ,"Hi", :an_atom]
 {1, "Hi", :an_atom}
 
+# Variables
+
 first_var = 6
 second_var = 7.8
 third_var = "Hi"
+
+IO.puts first_var
+IO.puts second_var,
+IO.puts third_var
+
+# Pattern Matching
+
+[v_1, v_2] = [1, 2]
