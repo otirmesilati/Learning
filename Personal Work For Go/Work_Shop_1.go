9661c7169d86ad6e7748972c986efdfae9ff6742
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-08-01 14:22:38 +0300

    Go Patch
    comments

--- Personal Work For Go/Work_Shop_1.go
+++ Personal Work For Go/Work_Shop_1.go
@@ -1,5 +1,8 @@
 package main // Required in Go Along with the main function
 
+// single line comments //
+// multi line comments //
+
 import "fmt" // IO package (acting as a library)
 
 func main(){
