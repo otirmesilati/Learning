b7852659cf66f868d04ada395c22cec304a4ce6b
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-08-01 14:30:00 +0300

    Go Patch
    Variables

--- Personal Work For Go/Work_Shop_1.go
+++ Personal Work For Go/Work_Shop_1.go
@@ -5,8 +5,15 @@ package main // Required in Go Along with the main function
 
 import "fmt" // IO package (acting as a library)
 
-func main(){
+func main(){ // no spacing between main and parantheses! :(
+
+  // variable declerations and assignments //
+  var fir_var int16
+  var sec_var int = 4
+  sec_var = int(fir_var) 
+
   fmt.Println("Coming Soon~")
+  fmt.Println("Value of first_var is:", sec_var)
 }
 
   
