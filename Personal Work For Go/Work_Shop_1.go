2345d918786d31bab33ebb25ed88ce7dfc0123c0
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-08-01 11:28:02 +0300

    Go Patch
    some comments

--- Personal Work For Go/Work_Shop_1.go
+++ Personal Work For Go/Work_Shop_1.go
@@ -1,6 +1,6 @@
-package main
+package main // Required in Go Along with the main function
 
-import "fmt"
+import "fmt" // IO package (acting as a library)
 
 func main(){
   fmt.Println("Coming Soon~")
