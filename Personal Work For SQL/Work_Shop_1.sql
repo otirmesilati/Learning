d663a28997f51113fc9dace9911182a40356c0e6
Otir <otirmesi@gmail.com>
2024-08-02 19:18:23 +0300

    SQL Patch
    adding the major attribute

--- Personal Work For SQL/Work_Shop_1.sql
+++ Personal Work For SQL/Work_Shop_1.sql
@@ -2,7 +2,8 @@ create table student_table (
     -- defining student ID + it's primary
     student_id int primary key,
     -- defning name 
-    student_name varchar[20]
+    student_name varchar[20],
     -- dening major
-
+    student_major varchar [20]
+    
 );
