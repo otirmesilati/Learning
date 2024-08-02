2376d1538c793cd4b2553570dffcf454e68def34
Otir <otirmesi@gmail.com>
2024-08-02 18:35:14 +0300

    SQL Patch
    adding the name attribute

--- Personal Work For SQL/Work_Shop_1.sql
+++ Personal Work For SQL/Work_Shop_1.sql
@@ -1,6 +1,8 @@
 create table student_table (
     -- defining student ID + it's primary
     student_id int primary key,
-    -- 
+    -- defning name 
+    student_name varchar[20]
+    -- dening major
 
 );
