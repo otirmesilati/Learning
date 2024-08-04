aa59e6eed95761bf59b9b0d1abe09e9fda8cdde4
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-08-04 05:08:59 +0300

    Java Patch
    adding thread extending section logic

--- Personal Work For Java/Work_Shop_8.java
+++ Personal Work For Java/Work_Shop_8.java
@@ -2,12 +2,19 @@ public class Work_Shop_8 {
     
     // let's have some fun with threads //
 
-    // 1 technique of parallelism - creating runnable blocks
+    // 1st technique of parallelism - creating runnable blocks
+    // wrap it with a thread
     public interface my_Runnable extends Runnable {
     
         public void run();
         public boolean hi(); // prints hi
         
     }
-    
+
+    // 2nd technique of parallelism - threading
+    public class my_Thread extends Thread 
+    {
+        // ???
+    } 
+
 }
