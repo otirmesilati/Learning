3398ad1acbacdf00cefee268afd7fdb81caea13b
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-08-04 05:13:31 +0300

    Java Patch
    adding some of the logic for the runnable and thread

--- Personal Work For Java/Work_Shop_8.java
+++ Personal Work For Java/Work_Shop_8.java
@@ -1,9 +1,12 @@
 public class Work_Shop_8 {
     
+    // this section will be used for the basics of //
+    // inheritence, implementation, IO, files etc... //
+    
     // let's have some fun with threads //
 
     // 1st technique of parallelism - creating runnable blocks
-    // wrap it with a thread
+    // wrap it with a thread or an executor
     public interface my_Runnable extends Runnable {
     
         public void run();
@@ -14,7 +17,11 @@ public class Work_Shop_8 {
     // 2nd technique of parallelism - threading
     public class my_Thread extends Thread 
     {
-        // ???
+        // run method here //
     } 
+
+    // add a method here: //
+    // run the my_runnable wrapped in a thread //
+    // run the my_thread //
 
 }
