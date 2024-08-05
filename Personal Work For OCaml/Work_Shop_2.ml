6b1ecf2e2e5e57f1cb55a77857db3eefb06bb279
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-08-05 05:54:43 +0300

    OCaml Patch
    operators for different types

--- Personal Work For OCaml/Work_Shop_2.ml
+++ Personal Work For OCaml/Work_Shop_2.ml
@@ -3,4 +3,14 @@
 (*literals*)
 2;;
 3.4;;
+'h';;
+(*int_of_char 'h';; to find out ASCII value*)
 "Bye";;
+false;;
+
+(* Operators *)
+2 + 4;;
+3 * 2;;
+3.2 +. 2.0;; (* different operators for different types operators*)
+
+
