cca09e66352477b8682c3b1becc3224c3828948f
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-08-17 11:19:28 +0300

    Batch Patch
    Building the logic for handling
    commands page

--- Personal Work For Batch/Work_Shop_9.bat
+++ Personal Work For Batch/Work_Shop_9.bat
@@ -1,3 +1,16 @@
 :: this will be used for a tutorial
 
 :: comments
+
+:: system information - using built ins
+:: system information - using environment variables
+
+:: Currently stopped at commands page, 
+:: Started to categorize the keywords to categories
+:: such as seen with the sysinfo above, 
+:: make more categories such as changing directories
+:: editing texts maybe.. etc and then add the 
+:: proper code! :)
+
+:: https://www.tutorialspoint.com/batch_script/batch_script_commands.htm
+:: Commands
