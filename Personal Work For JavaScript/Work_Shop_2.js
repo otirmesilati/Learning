69e6c3d16303e7af9d98b3ee1149be7ac9985aec
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-08-18 06:33:34 +0300

    Java Script Patch
    commenting

---
+++ Personal Work For JavaScript/Work_Shop_2.js
@@ -0,0 +1,5 @@
+// comments
+/* multi- line comments */
+
+// https://www.w3schools.com/js/js_statements.asp
+// Js Statements (rolling message: redo HOME to output later after to connect js to HTML and CSS)
