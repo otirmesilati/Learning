b9045649e0d507294b65c21abd3f54ce8e41ac9b
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-08-18 20:15:04 +0300

    Julia Patch
    Commenting

---
+++ Personal Work For Julia/Work_Shop_2.jl
@@ -0,0 +1,9 @@
+# single line comments
+
+#=
+multi line
+comments
+=#
+
+# https://www.tutorialspoint.com/julia/index.htm
+# Home 
--- Personal Work For MDBQL/Work_Shop_1.js
+++ Personal Work For MDBQL/Work_Shop_1.js
@@ -1,1 +1,3 @@
 // We will mongo here //
+
+// https://www.youtube.com/watch?v=11phu1pEJm4 //
