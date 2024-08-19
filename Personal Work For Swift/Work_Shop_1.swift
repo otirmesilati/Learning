23256f7764dc7ea6f3dd2f00d26e5b45aa3668ab
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-08-19 20:45:08 +0300

    Swift Patch
    Commenting

--- Personal Work For Swift/Work_Shop_1.swift
+++ Personal Work For Swift/Work_Shop_1.swift
@@ -1,5 +1,20 @@
 //   & & & & & & Comments & & & & & & 
 
+// Single line comment
+
+/* Multi
+Line Comment
+*/
+
+/* 
+Nested 
+/* 
+Multi
+line
+*/
+comments! 
+*/
+
 //   & & & & & & Literals & & & & & & 
 
 //   & & & & & & Operators & & & & & & 
@@ -23,5 +38,8 @@
 
  // stopped at keywords 
  // https://www.tutorialspoint.com/swift/swift_basic_syntax.htm
+ 
+// type "swift" in a shell to open the REPL
+
  // emulator: 
  // https://swiftplayground.run/
