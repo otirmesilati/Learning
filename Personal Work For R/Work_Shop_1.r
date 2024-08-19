89032de4e85527ef9c5e8ca16914599b93294d3c
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-08-19 07:58:59 +0300

    R Patch
    first 6 standardization parts

--- Personal Work For R/Work_Shop_1.r
+++ Personal Work For R/Work_Shop_1.r
@@ -1,3 +1,17 @@
+#   & & & & & & Comments & & & & & & 
+
+#   & & & & & & Literals & & & & & & 
+
+#   & & & & & & Operators & & & & & & 
+
+#   & & & & & & String Formatting & & & & & & 
+
+#   & & & & & & Conditional Statements & & & & & & 
+
+#   & & & & & & Loops & & & & & & 
+
+
+
 # Literals 
 3
 -5.8
