51adfac9f25bc1d355e2849130775d3b07df5f06
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-08-19 23:43:55 +0300

    FullStack Patch
    Adding the port

--- FullStack/E-Shops/Work_Shop_1.js
+++ FullStack/E-Shops/Work_Shop_1.js
@@ -1,2 +1,10 @@
-// 5. Adding the express framework //
+// 5.1. Adding the express framework //
 import express from "express";
+// importing the port and using it //
+import { PORT } from "./config.js";
+
+// 5.2. Deining expwess variable //
+const app = express();
+
+// 5.3. declaring the port
+export const PORT = 5555;
