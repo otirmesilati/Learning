73922ea5174ab3be26d64dc0c2ad16d9e3b9e43c
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-08-20 08:51:27 +0300

    vb.NET Patch
    first six part standardizations

--- Personal Work For Visual Basic/Work_Shop_2.vb
+++ Personal Work For Visual Basic/Work_Shop_2.vb
@@ -1,3 +1,18 @@
+' https://www.tutorialspoint.com/vb.net/vb.net_program_structure.htm ' 
+' Program Structure '
+
+' & & & & & & Comments & & & & & & '
+
+' & & & & & & Literals & & & & & & '
+
+' & & & & & & Operators & & & & & & '
+
+' & & & & & & String Formatting & & & & & & '
+
+' & & & & & & Conditional Statements & & & & & & '
+
+' & & & & & & Loops & & & & & & '
+
 ' Single line comment '
 REM also a single line comment
 
@@ -6,5 +21,3 @@ REM also a single line comment
 ' comments 
 #End Region
 
-' https://www.tutorialspoint.com/vb.net/index.htm ' 
-' Home '
