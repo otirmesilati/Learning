01901dc80495c81ccf6697d2257a03e8a6c50476
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-08-22 16:10:28 +0300

    Ruby Patch
    Adding Integer Literals part

--- Personal Work For Ruby/Work_Shop_3.rb
+++ Personal Work For Ruby/Work_Shop_3.rb
@@ -13,7 +13,25 @@ Comments
 
 #   & & & & & & Literals & & & & & &  
 
-# ints
+#   ^ ^ ^ ^ ^ Integers ^ ^ ^ ^ ^
+123
+4_273
+-34
+
+=begin
+Fixnum? Bignum?
+Seperation between fixnum and bignum is NOT important
+As they are internally/encpasulately implemented and abstracted away
+from the user of the API
+=end
+
+#   ^ ^ ^ ^ Integers: Type Modifiers ^ ^ ^ ^
+0xF5
+0o12
+0b110
+
+#   ^ ^ ^ ^ ? ^ ^ ^ ^
+
 # floats
 # strings
 # arrays
