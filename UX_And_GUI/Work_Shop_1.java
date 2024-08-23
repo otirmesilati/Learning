096b7c2e1649e7eee702ab9aeab028c36c8e2ca7
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-08-23 08:36:45 +0300

    UX/GUI Patch
    Starting to delve into java's GUI tools

---
+++ UX_And_GUI/Work_Shop_1.java
@@ -0,0 +1,17 @@
+package UX_And_GUI;
+
+public class Work_Shop_1 {
+
+    public class GUIElements
+    {
+        /*   & & & & & & Libraries
+         java.awt - GUI tools library, platform dependent
+         java swing - built upon awt
+         */
+
+        //   & & & & & & Components
+
+        //   & & & & & & Components: java.swing
+    }
+
+}
