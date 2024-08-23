80cbc0a6832449df7332ca2590fefa74415343c8
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-08-23 06:01:10 +0300

    C++ Patch
    simple literals and modifiers

--- Personal Work For C++/Work_Shop_7.cpp
+++ Personal Work For C++/Work_Shop_7.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 
 // https://www.tutorialspoint.com/cplusplus/cpp_comments.htm
-// next : data types, syno: theres a lot of types, they have modifiers, from "The size of variables..."" 
+// next : signed, unsigned, short long notation with u/l/ll in the end 
+// data types, syno: theres a lot of types, they have modifiers, from "The size of variables..."" 
 
 // where to put bitmasking?
 
@@ -22,8 +23,25 @@ for other kind of comments */
 
 void fun_hi(){cout << "Hi" /* Comments within statemenets */ << "bye";}
 
+void fun_literals(){
 //   & & & & & & Literals & & & & & &
 
+// https://web.archive.org/web/20130807052041/http://cpp.comsci.us/etymology/literals.html
+
+
+//   ^ ^ ^ ^ ^ Integers ^ ^ ^ ^ ^
+123;
+// no underscore format! 
+-34;
+
+
+//  ^ ^ ^ ^ Integers: Type Modifiers ^ ^ ^ ^
+
+0xF5;
+012; // octal literals written without o //
+0b110;
+
+}
 
 //   & Types & (finish this)
 
