b484525b18e29e991aa5418e13104818fbdbdc64
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-08-23 06:27:40 +0300

    Java Patch
    Integer values

--- Personal Work For Java/Work_Shop_9.java
+++ Personal Work For Java/Work_Shop_9.java
@@ -1,7 +1,11 @@
 // https://www.w3schools.com/java/java_comments.asp
 // Syntax
 
-//   & & & & & & Comments & & & & & &   // 
+
+public class Work_Shop_9 {
+
+    
+    //   & & & & & & Comments & & & & & &   // 
 
 // Single Line Comment
 
@@ -9,21 +13,37 @@
 Comments
  */
 
-//   & & & & & & Literals & & & & & &   //
-
-// https://www.baeldung.com/java-literals for the basics
-// https://www.geeksforgeeks.org/literals-in-java/ for unicode and maybe more
-// https://www.javatpoint.com/literals-in-java for escape  keys
-
-//   & & & & & & Operators & & & & & &   // 
-
-//   & & & & & & String Formatting & & & & & &   //
-
-//   & & & & & & Conditional Statements & & & & & &   //
-
-//   & & & & & & Loops & & & & & &   //
-
-public class Work_Shop_9 {
+public void fun_literals(){
     
+    //   & & & & & & Literals (Values in java) & & & & & &   //
+    
+    // https://www.baeldung.com/java-literals for the basics
+    // https://www.geeksforgeeks.org/literals-in-java/ for unicode and maybe more
+    // https://www.javatpoint.com/literals-in-java for escape  keys
+    
+    //   ^ ^ ^ ^ ^ Integers ^ ^ ^ ^ ^
+    
+    // no literals in java, only assigment values
+
+    // 123;
+    //-34;
+    
+    //  ^ ^ ^ ^ Integers: Type Modifiers ^ ^ ^ ^
+    
+    // 0xF5;
+    // 012;
+    // 0b110;
+    
+    }
+    
+    //   & & & & & & Operators & & & & & &   // 
+    
+    //   & & & & & & String Formatting & & & & & &   //
+    
+    //   & & & & & & Conditional Statements & & & & & &   //
+    
+    //   & & & & & & Loops & & & & & &   //
+    
+
 }
 
