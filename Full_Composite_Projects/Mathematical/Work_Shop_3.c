ccd632184b66bf94055ca3c16a725c5a62349b5d
Otyr <otirmesi@gmail.com>
2024-09-19 06:23:15 +0300

    Projects patch
    Adding the user stories portion

--- Full_Composite_Projects/Mathematical/Work_Shop_3.c
+++ Full_Composite_Projects/Mathematical/Work_Shop_3.c
@@ -1,3 +1,8 @@
-// Design - done (C)
-// pseudo - ? 
-// Assignment: Something that involves literals + variables + control flow without functions!
+
+// Variables: 
+
+// User Stroy 1: Simple counter
+// User Story 2: Danny's three purchases in the candy shop
+// User Story 3: 
+
+// Variables + Control Flow
