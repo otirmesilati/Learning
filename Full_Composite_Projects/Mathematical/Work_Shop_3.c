e010e5a74849a6c39435238ce3a08cda7982b60e
Otyr <otirmesi@gmail.com>
2024-09-19 06:29:43 +0300

    Projects patch
    Adding triangle creation's user story

--- Full_Composite_Projects/Mathematical/Work_Shop_3.c
+++ Full_Composite_Projects/Mathematical/Work_Shop_3.c
@@ -1,8 +1,9 @@
 
 // Variables: 
 
-// User Stroy 1: Simple counter
-// User Story 2: Danny's three purchases in the candy shop
-// User Story 3: 
+// User Stroy 1: Simple counter (1 incrementing varaible)
+// User Story 2: Danny's three purchases in the candy shop (1 math manipulated variable)
+// User Story 3: A Point in the Euclidean plane (2 match manipulated variables) 
 
-// Variables + Control Flow
+// Variables + Control Flow:
+// User Story 1: Triangle Creation (Third Edge bigger than the rest)
