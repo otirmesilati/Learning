4100b4f2158ef5a2eb70ced8bb67a5c7ae732bcb
Otyr <otirmesi@gmail.com>
2024-09-21 08:35:24 +0300

    C++ Patch
    Preparing the Inheritance portion

--- Personal Work For C++/Glossary.cpp
+++ Personal Work For C++/Glossary.cpp
@@ -1054,9 +1054,125 @@ private:
 // empty! //
 
 
+//
+
+// Three class dependency
+
+class My_Date
+{
+
+public:
+
+	My_Date(): year(2020), month(2), day (2) {}
+	My_Date(int year, int month, int day): year(year), month(month), day(day) {}
+	My_Date(const My_Date& other_date): year(other_date.year), month(other_date.month), day(other_date.day) {}
+
+	~My_Date() {}
+
+	My_Date& operator=(const My_Date& other_date)
+	{
+		if (this != &other_date)
+		{
+			year = other_date.year;
+			month = other_date.month;
+			day = other_date.day;
+		}
+		return *this;
+	}
+
+private:
+
+	int year, month, day;
+
+};
 
 
+class Employee1
+{
 
+public:
+
+	Employee1(): full_name(nullptr), hiring_date(), id(NULL), department_id(NULL) {}
+	Employee1(const Employee1& other_employee) : full_name(nullptr), hiring_date(other_employee.hiring_date), id(other_employee.id), department_id(other_employee.department_id)
+	{
+		if (other_employee.full_name)
+		{
+			int name_length = 0;
+			while (other_employee.full_name[name_length] != '\0') ++name_length;
+			full_name = new char[name_length];
+			for (int init_index = 0; init_index < name_length; ++init_index) full_name[init_index] = other_employee.full_name[init_index];
+		}
+	}
+
+	~Employee1() { delete full_name; }
+
+	Employee1 operator=(const Employee1& other_employee)
+	{
+		if (this != &other_employee)
+		{
+			delete full_name;
+			if (other_employee.full_name)
+			{
+				int name_length = 0;
+				while (other_employee.full_name[name_length] != '\0') ++name_length;
+				full_name = new char[name_length];
+				for (int copy_index = 0; copy_index < name_length; ++copy_index) full_name[copy_index] = other_employee.full_name[copy_index];
+			}
+		}
+		return *this;
+	}
+
+	void set_hiring_date(int y, int m, int d) { hiring_date = My_Date(y,m,d); }
+	void set_department(short dep_id) { department_id = dep_id; }
+
+private:
+
+	char* full_name;
+	My_Date hiring_date;
+	int id;
+	short department_id;
+
+};
+
+class Manager1 // : public Employee1 to inherit from empoyee
+{
+
+public:
+
+	Manager1(): emp_part(), workers(nullptr), number_of_workers(NULL)  {}
+	Manager1(const Manager1& other_manager): emp_part(other_manager.emp_part), workers(nullptr), number_of_workers(other_manager.number_of_workers)
+	{
+		if (other_manager.workers)
+		{
+			workers = new Employee1[number_of_workers];
+			for (int init_index = 0; init_index < number_of_workers; ++init_index) workers[init_index] = other_manager.workers[init_index];
+		}
+	}
+
+	~Manager1() { delete workers; }
+
+	Manager1& operator=(const Manager1& other_manager)
+	{
+		if (this != &other_manager)
+		{
+			delete workers;
+			if (other_manager.workers)
+			{
+				number_of_workers = other_manager.number_of_workers;
+				workers = new Employee1[number_of_workers];
+				for (int copy_index = 0; copy_index < number_of_workers; ++copy_index) workers[copy_index] = other_manager.workers[copy_index];
+			}
+		}
+		return *this;
+	}
+
+private:
+
+	Employee1 emp_part;
+	Employee1* workers;
+	int number_of_workers;
+
+};
 
 
 
