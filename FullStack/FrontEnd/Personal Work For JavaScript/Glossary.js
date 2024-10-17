11a35f1616a344d4eeb83702fa726c716a60026d
Otyr <otirmesi@gmail.com>
2024-10-17 16:23:25 +0300

    Assets

--- FullStack/FrontEnd/Personal Work For JavaScript/Glossary.js
+++ FullStack/FrontEnd/Personal Work For JavaScript/Glossary.js
@@ -147,175 +147,155 @@ function fun_operators() {
 //   & & & & & & Variables & & & & & &
 // type hinting? (static - no, dynamic - yes)
 
-/*
-void fun_variables()
-{
+function fun_variables() {
+  //bool my_bool;
+  //char my_char;
+  //wchar_t my_wchar;
+  //int my_int;
+  //long my_long;
+  //float my_float;
+  //double my_double;
+  //int imagine_this_is_out_of_any_function_global = 0;
 
-bool my_bool;
-char my_char;
-wchar_t my_wchar;
-int my_int;
-long my_long;
-float my_float;
-double my_double;
-int imagine_this_is_out_of_any_function_global = 0;
+  // unsigned short cap: 65535
+  //unsigned short hi = 65000; // 65,00
+  //unsigned short hi = 66000; // 464 (Got back to zero going up)
 
-// unsigned short cap: 65535
-unsigned short hi = 65000; // 65,00
-unsigned short hi = 66000; // 464 (Got back to zero going up)
+  // signed short cap: 32767
+  //short hi = 32000; // 32000
+  //short hi = 35000; // -30536 (Got to -32767 going to zero)
 
-// signed short cap: 32767
-short hi = 32000; // 32000
-short hi = 35000; // -30536 (Got to -32767 going to zero)
+  //   ^ ^ ^ ^ ^ Scope Sharing ^ ^ ^ ^ ^
 
+  //   ^ ^ ^ ^ Global & Static Variables ^ ^ ^ ^
+  // Both default value if not initialized : 0
 
-//   ^ ^ ^ ^ ^ Scope Sharing ^ ^ ^ ^ ^
+  //imagine_this_is_out_of_any_function_global = 4;
 
-//   ^ ^ ^ ^ Global & Static Variables ^ ^ ^ ^
-// Both default value if not initialized : 0
+  {
+    // imagine this is void function my_func
+    //  printf("global is accessible: %d", imagine_this_is_out_of_any_function_global);
+    //  imagine_this_is_out_of_any_function_global = 7; // and mutable //
+    //  static int my_static = 0; // static variable accessible in my_func
+  }
 
-imagine_this_is_out_of_any_function_global = 4;
+  // but even though it's lifetime is the same as the global variable - trying to access the static variable from main - is a compilation error
 
-{ // imagine this is void function my_func
+  //   ^ ^ ^ ^ Local variables ^ ^ ^ ^
 
-    printf("global is accessible: %d", imagine_this_is_out_of_any_function_global);    
-    imagine_this_is_out_of_any_function_global = 7; // and mutable //
-    static int my_static = 0; // static variable accessible in my_func
-}  
-
-// but even though it's lifetime is the same as the global variable - trying to access the static variable from main - is a compilation error
-
-
-//   ^ ^ ^ ^ Local variables ^ ^ ^ ^
-
-int first_level_local = 1;
-int this_wont_be_changed_local = 2;
-{ 
-
-    int second_level_local = 3;
-    int this_wont_be_changed_local = 4;
+  //int first_level_local = 1;
+  //int this_wont_be_changed_local = 2;
+  {
+    //  int second_level_local = 3;
+    //  int this_wont_be_changed_local = 4;
 
     {
-        int third_level_local = 5;
+      //    int third_level_local = 5;
     }
+  }
 
+  //first_level_local = 3;
+  // second_level_local = 4; -> not possible, it's local to the block
+  // this_wont_be_changed_local -> 2, the one with the 4 value was a different variable
+
+  //   ^ ^ ^ ^ ^ Constants ^ ^ ^ ^ ^
+
+  //   ^ ^ ^ ^ Pre-Processor Macros ^ ^ ^ ^
+
+  //   ^ ^ ^ Defines ^ ^ ^
+
+  //#define CRT_SECURE_NO_WARNINGS
+  //#define ZERO 0
+  //#define PI 3.1415
+
+  //   ^ ^ ^ Enums(Almost pre-processor) ^ ^ ^
+
+  //enum boolean {NO, YES};
+  //typedef enum {TRUE = 1, FALSE = 0} Boolean;
+
+  //   ^ ^ ^ ^ Constants: Immutables
+
+  //   ^ ^ ^ ConstExpr : Compile Time Immutable ^ ^ ^
+
+  //constexpr int static_time_const = 0;
+
+  //   ^ ^ ^ Const : Run Time Immutable ^ ^ ^
+
+  //const int dynamic_time_const = 1;
+
+  //   ^ ^ Pointer Const ^ ^
+
+  //int mutable_value = 0;
+  //int immutable_value = 1;
+  //int immutable_value_and_adress = 2;
+
+  //const int* const_pointer_to_int = &immutable_value;
+  //int* const const_pointer_to_int = &mutable_value;
+  //const int* const const_pointer_to_int = &immutable_value_and_adress;
+
+  //   ^ ^ Reference Const ^ ^
+
+  //const int& const_ref_to_int = immutable_value;
+
+  //   ^ ^ ^ ^ ^ Pointers ^ ^ ^ ^ ^
+
+  //char* my_char_pointer;
+  //int* my_int_pointer;
+  //long* my_long_pointer;
+  //float* my_float_pointer;
+  //double* my_double_pointer;
+
+  //   ^ ^ ^ ^ Static Allocation ^ ^ ^ ^
+
+  //my_char_pointer = &my_char;
+  //my_int_pointer = &my_int;
+  //my_long_pointer = &my_long;
+  //my_float_pointer = &my_float;
+  //my_double_pointer = &my_double;
+
+  //   ^ ^ ^ ^ Dynamic Allocation ^ ^ ^
+
+  //   ^ ^ ^ Heap Byte Allocation: Malloc ^ ^ ^
+
+  //my_char_pointer = new char;
+  //my_int_pointer = new int;
+  //my_long_pointer = new long;
+  //my_float_pointer = new float;
+
+  //   ^ ^ ^ Heap Memory De-Allocation ^ ^ ^
+
+  //delete my_char_pointer;
+  //delete my_int_pointer;
+  //delete my_long_pointer;
+  //delete my_float_pointer;
+  //delete my_double_pointer;
+
+  //   ^ ^ ^ ^ Void (Generaic) Pointers ^ ^ ^ ^
+
+  //void* my_generic_pointer;
+
+  //char* my_generic_pointer_converted_to_char_pointer;
+  //int* my_generic_pointer_converted_to_int_pointer;
+  //long* my_generic_pointer_converted_to_long_pointer;
+  //float* my_generic_pointer_converted_to_float_pointer;
+  //double* my_generic_pointer_converted_to_double_pointer;
+
+  //my_generic_pointer_converted_to_char_pointer = (char*)my_generic_pointer;
+  //my_generic_pointer_converted_to_int_pointer = (int*)my_generic_pointer;
+  //my_generic_pointer_converted_to_long_pointer = (long*)my_generic_pointer;
+  //my_generic_pointer_converted_to_float_pointer = (float*)my_generic_pointer;
+  //my_generic_pointer_converted_to_double_pointer = (double*)my_generic_pointer;
+
+  //   ^ ^ ^ ^ ^ Data Collections ^ ^ ^ ^ ^
+
+  // strings?
+
+  //   ^ ^ ^ ^ ^ References ^ ^ ^ ^ ^
+
+  //int& my_reference = mutable_value;
 }
 
-first_level_local = 3;
-// second_level_local = 4; -> not possible, it's local to the block
-// this_wont_be_changed_local -> 2, the one with the 4 value was a different variable
-
-
-//   ^ ^ ^ ^ ^ Constants ^ ^ ^ ^ ^
-
-//   ^ ^ ^ ^ Pre-Processor Macros ^ ^ ^ ^
-
-//   ^ ^ ^ Defines ^ ^ ^
-
-#define CRT_SECURE_NO_WARNINGS
-#define ZERO 0
-#define PI 3.1415
-
-
-//   ^ ^ ^ Enums(Almost pre-processor) ^ ^ ^
-
-enum boolean {NO, YES};
-typedef enum {TRUE = 1, FALSE = 0} Boolean;
-
-
-//   ^ ^ ^ ^ Constants: Immutables 
-
-//   ^ ^ ^ ConstExpr : Compile Time Immutable ^ ^ ^ 
-
-//constexpr int static_time_const = 0;
-
-
-//   ^ ^ ^ Const : Run Time Immutable ^ ^ ^
-
-const int dynamic_time_const = 1;
-
-//   ^ ^ Pointer Const ^ ^
-
-int mutable_value = 0;
-int immutable_value = 1;
-int immutable_value_and_adress = 2;
-
-const int* const_pointer_to_int = &immutable_value;
-int* const const_pointer_to_int = &mutable_value;
-const int* const const_pointer_to_int = &immutable_value_and_adress;
-
-//   ^ ^ Reference Const ^ ^ 
-
-const int& const_ref_to_int = immutable_value;
-
-//   ^ ^ ^ ^ ^ Pointers ^ ^ ^ ^ ^
-
-char* my_char_pointer;
-int* my_int_pointer;
-long* my_long_pointer;
-float* my_float_pointer;
-double* my_double_pointer;
-
-//   ^ ^ ^ ^ Static Allocation ^ ^ ^ ^
-
-my_char_pointer = &my_char;
-my_int_pointer = &my_int;
-my_long_pointer = &my_long;
-my_float_pointer = &my_float;
-my_double_pointer = &my_double;
-
-//   ^ ^ ^ ^ Dynamic Allocation ^ ^ ^
-
-//   ^ ^ ^ Heap Byte Allocation: Malloc ^ ^ ^
-
-my_char_pointer = new char;
-my_int_pointer = new int;
-my_long_pointer = new long;
-my_float_pointer = new float;
-
-//   ^ ^ ^ Heap Memory De-Allocation ^ ^ ^
-
-delete my_char_pointer;
-delete my_int_pointer;
-delete my_long_pointer;
-delete my_float_pointer;
-delete my_double_pointer;
-
-//   ^ ^ ^ ^ Void (Generaic) Pointers ^ ^ ^ ^
-
-void* my_generic_pointer;
-
-char* my_generic_pointer_converted_to_char_pointer;
-int* my_generic_pointer_converted_to_int_pointer;
-long* my_generic_pointer_converted_to_long_pointer;
-float* my_generic_pointer_converted_to_float_pointer;
-double* my_generic_pointer_converted_to_double_pointer;
-
-my_generic_pointer_converted_to_char_pointer = (char*)my_generic_pointer;
-my_generic_pointer_converted_to_int_pointer = (int*)my_generic_pointer;
-my_generic_pointer_converted_to_long_pointer = (long*)my_generic_pointer;
-my_generic_pointer_converted_to_float_pointer = (float*)my_generic_pointer;
-my_generic_pointer_converted_to_double_pointer = (double*)my_generic_pointer;
-
-//   ^ ^ ^ ^ ^ Data Collections ^ ^ ^ ^ ^ 
-
-// strings?
-
-
-//   ^ ^ ^ ^ ^ References ^ ^ ^ ^ ^
-
-int& my_reference = mutable_value;
-
-}
-
-
-
-
-
-
-
-
-
+/*
 //   & & & & & & Flow Control & & & & & &
 
 
