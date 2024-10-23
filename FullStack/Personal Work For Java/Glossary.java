41772e67df4bf03eb27b2cd2412999b0f3ba5dfd
Otyr <otirmesi@gmail.com>
2024-10-23 14:45:27 +0300

    Assets

--- FullStack/Personal Work For Java/Glossary.java
+++ FullStack/Personal Work For Java/Glossary.java
@@ -1051,7 +1051,12 @@ int switch_key = 2;
 
 
 //   < < < < < < < <  GUI Tools   > > > > > > > >   
-//         // https://examples.javacodegeeks.com/java-swing-layouts-example/
+// https://examples.javacodegeeks.com/java-swing-layouts-example/
+// https://web.mit.edu/6.005/www/sp14/psets/ps4/java-6-tutorial/components.html
+
+//         // C GUI: https://www.wxwidgets.org/
+//         // Python GUI: https://docs.python.org/3/library/tkinter.html
+
 
 
 
@@ -1065,13 +1070,16 @@ public void guiElements()
 
 //   & & & & & & GUI Components & & & & & &
 
-//   ^ ^ ^ ^ ^ JFrame ^ ^ ^ ^ ^
+//   ^ ^ ^ ^ ^ Windows ^ ^ ^ ^ ^
 
-JFrame myJFrame = new JFrame();
+JFrame myJFrame = new JFrame(); 
+JDialog myJDialog = new JDialog(); // Alert window
 
 //   ^ ^ ^ ^ ^ Panel Panes ^ ^ ^ ^ ^
 
-//   ^ ^ ^ ^ ^ Layouts ^ ^ ^ ^ ^
+JPanel myJPanel = new JPanel();
+
+//   ^ ^ ^ ^ ^ Panel Layouts ^ ^ ^ ^ ^
 
 // FlowLayout
 // BorderLayout        
@@ -1083,109 +1091,50 @@ JFrame myJFrame = new JFrame();
 // Overlay
 // Spring
 
+//   ^ ^ ^ ^ ^ Menu ^ ^ ^ ^ ^
 
+JMenu myJMenu = new JMenu();
 
-// panes -> layouts -> ?
+//   ^ ^ ^ ^ ^ Buttons ^ ^ ^ ^ ^
+
+JButton myJButton = new JButton();
+JRadioButton myRadioButton = new JRadioButton(); // circled check mark
+JCheckBox MyCheckBox = new JCheckBox();
+
+//   ^ ^ ^ ^ ^ Text filling ^ ^ ^ ^ ^
+
+JTextField myTextField = new JTextField();
+JPasswordField myPassword = new JPasswordField(); // hides the symbols in dots
+
+//   ^ ^ ^ ^ ^ Labels ^ ^ ^ ^ ^
+
+//   ^ ^ ^ ^ ^ Label + DropDown : ComboBox ^ ^ ^ ^ ^ 
+
+JComboBox myComboBox = new JComboBox<>();
+
+//   ^ ^ ^ ^ ^ DropDown List ^ ^ ^ ^ ^
+
+JList myJList = new JList<>();
 
 }
 
 
 
 
-
-
-
-//         JPanel myJPanel = new JPanel();
-
-
-
-
-        
-
-
-//          // Main window
-//         JDialog myJDialog = new JDialog(); // sub-window, of a JFrame usually, classicaly meant for alerts
-
-//         //   ^ ^ ^ ^ Components (Window Elements) ^ ^ ^ ^
-//         // https://web.mit.edu/6.005/www/sp14/psets/ps4/java-6-tutorial/components.html
-
-//         //   ^ ^ ^ Components: Menu ^ ^ ^
-
-//         //   ^ ^ Menu: JMenu ^ ^ //
-//         JMenu myJMenu = new JMenu();
-
-//         //   ^ ^ ^  Components: Clickers ^ ^ ^
-
-//         //   ^ ^ Clickers: JButton ^ ^ - simple push button 
-//         JButton myJButton = new JButton();
-
-//         //   ^ ^ Clickers: JRadioButton - circular check button ^ ^
-//         JRadioButton myRadioButton = new JRadioButton();
-
-//         //   ^ ^ Clickers: JCheckBox - v sign check button ^ ^
-//         JCheckBox MyCheckBox = new JCheckBox();
-
-
-//         //   ^ ^ ^ Components : Text Spaces ^ ^ ^
-
-//         //   ^ ^ Text Spaces: JTextField ^ ^
-
-//         JTextField myTextField = new JTextField();
-
-//         //   ^ ^ Text Spaces: JPasswordField - letters replaced by dots ^ ^
-        
-//         JPasswordField myPassword = new JPasswordField();
+         
 
 //         //   ^ ^ Text Spaces: JSpinner - added up/down arrow buttons ^ ^
 //         JSpinner mySpinner = new JSpinner();
-
-
-//         //   ^ ^ ^ Components: DropDowns ^ ^ ^ //
-
-//         //   ^ ^ DropDowns : JMenu ^ ^ - upper ruler menu + dropdown //
-//         JMenu myMenu = new JMenu();
-
-//         //   ^ ^ DropDowns : JComboBox - SelectionBar + DropDown ^ ^ - upper ruler menu + dropdown //
-//         JComboBox myComboBox = new JComboBox(); // needs to add items // 
-
-//         //   ^ ^ DropDowns : JList ^ ^ - Classic dropdown //
-//         JList MyDropList = new JList(); // needs to add items //
-        
-        
-//         //   ^ ^ ^ Components: Bars ^ ^ ^ //
 
 //         //   ^ ^ Bars : JProgressBar ^ ^ - ? //
         
 //         //   ^ ^ Bars : JSlider ^ ^ - ? //
 
-//         // labels ? //
-//         // textfields ? //
-
-//         // Java GUI: https://web.mit.edu/6.005/www/sp14/psets/ps4/java-6-tutorial/components.html
-//         // C GUI: https://www.wxwidgets.org/
-//         // Python GUI: https://docs.python.org/3/library/tkinter.html
-//         // 
-
-
 //         //   ^ ^ DropDowns : JList ^ ^ - Classic dropdown //
 
 //         //   ^ ^ ^ Components : Boxes ^ ^ ^
 
-//         //   ^ ^ ^ Components : Boxes ^ ^ ^
-
-//         //   ^ ^ ^ Components : Boxes ^ ^ ^
-
 //         //   ^ ^ ^ ^ java specific(?): callbacking GUI ^ ^ ^ ^
-//     }
-
-//     public class LoginWindow
-//     {
-//         // So here will the the code for making a Login Window
-
-        
-
-//     }
-
     
 //     /*   & & & & & & GUI dependencies & & & & & */
 
@@ -1200,7 +1149,7 @@ JFrame myJFrame = new JFrame();
 //     // is this class static or not? (getting a "free" pointer as this/self) //
 //     // is this class inheriting? maybe a friend class? //
 //     // interfaces for behaviour? //
-// }
+
 
 
 
