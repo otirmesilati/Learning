f1168f27b81aa253729f46ddb711e1db08aecc6d
Otyr <otirmesi@gmail.com>
2024-11-09 19:28:56 +0200

    Assets

--- DevOps/SysAdmin/OS & Virtual Machines/Automations & Scripting Languages/Personal Work For JavaScript/Glossary.js
+++ DevOps/SysAdmin/OS & Virtual Machines/Automations & Scripting Languages/Personal Work For JavaScript/Glossary.js
@@ -246,319 +246,196 @@ function fun_variables() {
 
   //   ^ ^ ^ ^ Block Nesting : Scope levels, Data Hiding and Shadowing ^ ^ ^ ^
 
-  //int first_level_local = 1;
-  //int this_wont_be_changed_local = 2;
+  var first_level_local = 1;
+  var this_wont_be_changed_local = 2;
   {
-    //  int second_level_local = 3;
-    //  int this_wont_be_changed_local = 4;
+    var second_level_local = 3; // Variable Hiding in local scope level
+    var this_wont_be_changed_local = 4; // Variable Shadowing (a new variable!) locally scoped
 
     {
-      //    int third_level_local = 5;
+      var third_level_local = 5;
     }
   }
 
-  //first_level_local = 3;
+  first_level_local = 3;
   // second_level_local = 4; -> not possible, it's local to the block
   // this_wont_be_changed_local -> 2, the one with the 4 value was a different variable
 
   //   ^ ^ ^ ^ ^ Constants ^ ^ ^ ^ ^
 
-  //   ^ ^ ^ ^ Pre-Processor Macros ^ ^ ^ ^
-
-  //   ^ ^ ^ Defines ^ ^ ^
-
-  //#define CRT_SECURE_NO_WARNINGS
-  //#define ZERO 0
-  //#define PI 3.1415
-
-  //   ^ ^ ^ Enums(Almost pre-processor) ^ ^ ^
-
-  //enum boolean {NO, YES};
-  //typedef enum {TRUE = 1, FALSE = 0} Boolean;
+  // No Pre-Processor directives and Enums in JavaScript
 
   //   ^ ^ ^ ^ Constants: Immutables
 
-  //   ^ ^ ^ ConstExpr : Compile Time Immutable ^ ^ ^
+  // No constexpr as of the dynamic typability of the language
 
-  //constexpr int static_time_const = 0;
+  //   ^ ^ ^ Const : immutable primitives and constant pointer for data collections ^ ^ ^
 
-  //   ^ ^ ^ Const : Run Time Immutable ^ ^ ^
+  const dynamic_time_const = 1;
+  const mutable_array = [2, 3, 4];
 
-  //const int dynamic_time_const = 1;
-
-  //   ^ ^ Pointer Const ^ ^
-
-  //int mutable_value = 0;
-  //int immutable_value = 1;
-  //int immutable_value_and_adress = 2;
-
-  //const int* const_pointer_to_int = &immutable_value;
-  //int* const const_pointer_to_int = &mutable_value;
-  //const int* const const_pointer_to_int = &immutable_value_and_adress;
-
-  //   ^ ^ Reference Const ^ ^
-
-  //const int& const_ref_to_int = immutable_value;
-
-  //   ^ ^ ^ ^ ^ Pointers ^ ^ ^ ^ ^
-
-  //char* my_char_pointer;
-  //int* my_int_pointer;
-  //long* my_long_pointer;
-  //float* my_float_pointer;
-  //double* my_double_pointer;
-
-  //   ^ ^ ^ ^ Static Allocation ^ ^ ^ ^
-
-  //my_char_pointer = &my_char;
-  //my_int_pointer = &my_int;
-  //my_long_pointer = &my_long;
-  //my_float_pointer = &my_float;
-  //my_double_pointer = &my_double;
-
-  //   ^ ^ ^ ^ Dynamic Allocation ^ ^ ^
-
-  //   ^ ^ ^ Heap Byte Allocation: Malloc ^ ^ ^
-
-  //my_char_pointer = new char;
-  //my_int_pointer = new int;
-  //my_long_pointer = new long;
-  //my_float_pointer = new float;
-
-  //   ^ ^ ^ Heap Memory De-Allocation ^ ^ ^
-
-  //delete my_char_pointer;
-  //delete my_int_pointer;
-  //delete my_long_pointer;
-  //delete my_float_pointer;
-  //delete my_double_pointer;
-
-  //   ^ ^ ^ ^ Void (Generaic) Pointers ^ ^ ^ ^
-
-  //void* my_generic_pointer;
-
-  //char* my_generic_pointer_converted_to_char_pointer;
-  //int* my_generic_pointer_converted_to_int_pointer;
-  //long* my_generic_pointer_converted_to_long_pointer;
-  //float* my_generic_pointer_converted_to_float_pointer;
-  //double* my_generic_pointer_converted_to_double_pointer;
-
-  //my_generic_pointer_converted_to_char_pointer = (char*)my_generic_pointer;
-  //my_generic_pointer_converted_to_int_pointer = (int*)my_generic_pointer;
-  //my_generic_pointer_converted_to_long_pointer = (long*)my_generic_pointer;
-  //my_generic_pointer_converted_to_float_pointer = (float*)my_generic_pointer;
-  //my_generic_pointer_converted_to_double_pointer = (double*)my_generic_pointer;
-
-  //   ^ ^ ^ ^ ^ Data Collections ^ ^ ^ ^ ^
-
-  // is there string comprehension in javascript?
-
-  // strings?
-
-  //   ^ ^ ^ ^ ^ References ^ ^ ^ ^ ^
-
-  //int& my_reference = mutable_value;
+  //   No Pointers & References in JavaScript
 }
 
-/*
 //   & & & & & & Flow Control & & & & & &
 
+function fun_flow_control() {
+  var condition = 1;
+  var outer_condition = 0;
+  var elif_condition = 1;
+  var if_condition = 0;
+  var elseif_condition = 1;
+  var switch_key = 2;
 
-void fun_flow_control()
-{
+  //   ^ ^ ^ ^ ^ Statements ^ ^ ^ ^ ^
 
-int condition = 1;
-int outer_condition = 0;
-int elif_condition = 1;
-int if_condition = 0; int elseif_condition = 1;
-int switch_key = 2;
+  //   ^ ^ ^ ^ ^ Labels (no GoTo in JavaScript) ^ ^ ^ ^ ^
 
-
-//   ^ ^ ^ ^ ^ Statements ^ ^ ^ ^ ^ 
-
-//   ^ ^ ^ ^ ^ Procedure labels & GoTo ^ ^ ^ ^ ^
-
-procedure_1_label:
-// first procedure  //
-
-goto procedure_2_label;
-
-procedure_2_label:
-// second procedure //
-
-// More Material: https://www.geeksforgeeks.org/local-labels-in-c/
-
-
-//   ^ ^ ^ ^ ^ Conditional Statements & & & & & &
-
-//   ^ ^ ^ ^ If & Else ^ ^ ^ ^ ^
-
-if(condition)
-{
-    // condition TRUE -> first procedure
-}
-
-else
-{
-    // condition FALSE -> second procedure
-}
-
-
-//   ^ ^ ^ Null Checks ^ ^ ^
-
-int val_of_ptr = 0;
-int* ptr_to_check = &val_of_ptr;
-
-// Alternative 1 - using 0 representations
-
-if (ptr_to_check!= 0) {}
-if (ptr_to_check!= NULL) {} 
-if (ptr_to_check!= '\0') {}
-
-// Alternative 2 - using zero equating
-
-if (ptr_to_check) {}
-
-
-//   ^ ^ ^ Nested Ifs ^ ^ ^
-
-if(outer_condition)
-{
-    // first procedure
-}
-else
-{
-    if(elif_condition) // else if logic , same as the else if example below
-    {
-        // second procedure
+  labels_are_only_before_for_loops: for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (i === 1 && j === 1) {
+        console.log("Breaking out of the outer loop");
+        break labels_are_only_before_for_loops; // Breaks the outer loop, not just the inner one
+      }
+      console.log(`i = ${i}, j = ${j}`);
     }
-}
+  }
 
+  //   ^ ^ ^ ^ ^ Conditional Statements & & & & & &
 
-//   ^ ^ ^ Else-If ^ ^ ^ 
+  //   ^ ^ ^ ^ If & Else ^ ^ ^ ^ ^
 
-if(if_condition)
-{
+  if (condition) {
+    // condition TRUE -> first procedure
+  } else {
+    // condition FALSE -> second procedure
+  }
+
+  //   ^ ^ ^ Null Checks ^ ^ ^
+
+  // Alternative 1 - using 0 representations
+
+  if (ptr_to_check != 0) {
+  }
+  if (ptr_to_check != NULL) {
+  }
+  if (ptr_to_check != "\0") {
+  }
+
+  // Alternative 2 - using zero equating
+
+  if (ptr_to_check) {
+  }
+
+  //   ^ ^ ^ Nested Ifs ^ ^ ^
+
+  if (outer_condition) {
     // first procedure
-}
-else if(elseif_condition) // same as the example above
-{
-    // second procedure
-}
+  } else {
+    if (elif_condition) {
+      // else if logic , same as the else if example below
+      // second procedure
+    }
+  }
 
+  //   ^ ^ ^ Else-If ^ ^ ^
+
+  if (if_condition) {
+    // first procedure
+  } else if (elseif_condition) {
+    // same as the example above
+    // second procedure
+  }
+}
 
 // map to the tablet - new skill for relations, building a "order relation" graph under constraints (number of maximum relation checks on members, like finding the max of 3 numbers(linear relation) in no more than 3 tries)
 // and also - mapping the control flow diagram - a straight line digram that splits whenever theres a conditional statement:
 //             - - - - - - - -
 //    - - - - -| condition 1  | - - - - - (when theres else-if the split is more than two - 3 + )
-//             - - - - - - - - 
+//             - - - - - - - -
 
 //   ^ ^ ^ ^ Switch ^ ^ ^ ^ ^
 
-switch (switch_key)
-{
-
-case 1: // first procedure
+switch (switch_key) {
+  case 1: // first procedure
     break;
 
-case 2: // second procedure
+  case 2: // second procedure
     break;
 
-case 3: // third procedure
+  case 3: // third procedure
     break;
 
-default: // fourth procedure
+  default: // fourth procedure
     break;
 
+    //   ^ ^ ^ ^ ^ Loops ^ ^ ^ ^ ^
+
+    // break? continue?
+
+    //   ^ ^ ^ ^ While Loops ^ ^ ^ ^
+
+    var while_index = 0;
+    while (while_index <= 5) {
+      // condition checking BEFORE the procedure ran
+      ++while_index;
+      // while procedure
+    }
+
+    //   ^ ^ ^ Do-While Loops ^ ^ ^
+
+    var do_while_index = -1;
+    do {
+      ++do_while_index;
+      // do-while procedure
+    } while (do_while_index <= 5); //condition checking AFTER the procedure ran
+
+    //   ^ ^ ^ ^ For Loops ^ ^ ^ ^
+
+    for (
+      var for_index = 0;
+      for_index <= 5;
+      ++for_index // same as above examples
+    ) {
+      // for procedure
+    }
+
+  //   ^ ^ ^ ^ Empty portions in for loop ^ ^ ^ ^
+  // no init? not condition? no counter?
+
+  // for(;;)
+  // for(;;)
+  // for(;;)
 }
 
+//   ^ ^ ^ ^ ^ Functions ^ ^ ^ ^ ^
 
-//   ^ ^ ^ ^ ^ Loops ^ ^ ^ ^ ^ 
+function my_function() {}
 
-// break? continue?
+//   ^ ^ ^ ^ Function hoisting - ReWrites Functions at the Top ^ ^ ^ ^
 
-//   ^ ^ ^ ^ While Loops ^ ^ ^ ^
+void_hoist_func();
+function void_hoist_func() {} // function declaration comes after - gets rewritten before
 
-int while_index = 0;
-while(while_index <= 5) // condition checking BEFORE the procedure ran
-{
-    ++while_index;
-    // while procedure
-}
+//   ^ ^ ^ ^  Argument Passing ^ ^ ^ ^
 
-//   ^ ^ ^ Do-While Loops ^ ^ ^
+//   No type passing, primitives passed by value, objects by reference
 
-int do_while_index = -1;
-do
-{
-    ++do_while_index;
-    // do-while procedure
-} while(do_while_index <= 5); //condition checking AFTER the procedure ran
-
-
-//   ^ ^ ^ ^ For Loops ^ ^ ^ ^
-
-for(int for_index = 0; for_index <= 5 ; ++for_index) // same as above examples
-{
-    // for procedure
-}
-
-
-
-//   ^ ^ ^ ^ Empty portions in for loop ^ ^ ^ ^
-// no init? not condition? no counter?
-
-// for(;;)
-// for(;;)
-// for(;;)
-
-}
-
-//   ^ ^ ^ ^ ^ Functions ^ ^ ^ ^ ^ 
-
-int int_return_function(){}
-// also_int_return_function(){} // defualt int type - though may cause a warning
-
-//   ^ ^ ^ ^ Function prototyping ^ ^ ^ ^
-
-void void_prtp_func_1(); // function declaration
-void void_prtp_func_2();
-// main procedure
-void void_prtp_func_1(){ return; } // function definition
-void void_prtp_func_2(){} // in void functions - the return statement may be omitted and the function will return in closing block
-
-// add to tablet: 1. static stack segment layout (building the stack) + dynamic stack segment allocation in function calls (changing the values and releasing the layout parts) 
-
-
-//   ^ ^ ^ ^  Argument Passing ^ ^ ^ ^ 
-
-//   ^ ^ ^ Passing By Value ^ ^ ^
-
-void pass_by_value(int val){}
-
-
-//   ^ ^ ^ Passing By Adress ^ ^ ^
-
-void pass_by_adress(int* adr){}
-// add this - pass_by_adress_2(int adr[]){} ? or in the array section ? 
-
-
-//   ^ ^ ^ Passing By Reference ^ ^ ^
-
-void pass_by_reference(int& ref){}
+function pass_by_value(val) {}
 
 // anon function? from version 11? what's the syntax?
 
 //   ^ ^ ^ ^ Recursive Functions ^ ^ ^ ^
 
-void rec_func(){rec_func();} // infinite loop logic
-
+function rec_func() {
+  rec_func();
+} // infinite loop logic
 
 //   ^ ^ ^ ^ Pointer Functions ^ ^ ^ ^
 // finish this from lecture 12
 
 //   ^ ^ ^ ^ Function Overloading ^ ^ ^ ^
-
+/*
 int overloaded_func(int val){}
 int overloaded_func(float val){} // function overloading - same function name but different parameters
 int overloaded_func(int val1, float val2){} // function overloading - same function name but different parameters
