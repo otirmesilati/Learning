d2b76ab10b9d81d8a696a0c3f766936c0723ae62
Otyr <otirmesi@gmail.com>
2024-11-13 11:54:01 +0200

    Assets

--- FullStack/Full_Composite_Projects/E-Shops/Work_Shop_1.bash
+++
@@ -1,8 +0,0 @@
-# 1. Initializing the project - React
-npm init -y 
-
-# 3. Express Frameworking + nodemon for restarting server on RT changes
-npm i express nodemon
-
-# 6.2 starting the server on selected port
-npm run dev
--- FullStack/Full_Composite_Projects/Gui_Projects/Work_Shop_1.java
+++
@@ -1,5 +0,0 @@
-package Full_Composite_Projects.Gui_Projects;
-
-public class Work_Shop_1 {
-    
-}
