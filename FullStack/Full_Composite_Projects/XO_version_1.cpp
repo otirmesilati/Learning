176e4e8d8f8c627e1d0701e2a128c6282279160f
Otyr <otirmesi@gmail.com>
2024-11-13 12:04:27 +0200

    Assets

---
+++ FullStack/Full_Composite_Projects/XO_version_1.cpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+// a function for setting the board
+
+// a function for checking if the winner won
+
+// a function for playing the game
+
+// main function
