c33a0503535c6de5e6fbe7d25b84a5be63ce9797
Otyr <otirmesi@gmail.com>
2024-11-18 07:07:51 +0200

    Assets

---
+++ DevOps/SysAdmin/Shells & Command Languages/Personal Work For PowerShell/Glossary.ps1
@@ -0,0 +1,12 @@
+ #   < < < < < < < <   Command Principles   > > > > > > > >   */
+
+
+ #   % % % % % % % Frame Handling % % % % % % %
+
+# Frame launching 
+# frame families
+# - windows utility apps
+# control panel... m(icrosoft)s(aved)c(onsole) apps...
+# frame categories
+# - editors, ides...
+# -- ...
