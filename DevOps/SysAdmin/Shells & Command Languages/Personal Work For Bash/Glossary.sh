dcada07b90ffa20d1239505221f2381ee05b3007
Otyr <45825931+otirmesilati@users.noreply.github.com>
2025-02-07 16:22:34 +0200

    Assets

--- DevOps/SysAdmin/Shells & Command Languages/Personal Work For Bash/Glossary.sh
+++ DevOps/SysAdmin/Shells & Command Languages/Personal Work For Bash/Glossary.sh
@@ -809,7 +809,7 @@ exit
 
 #   ^ ^ ^ ^ ^ Variable input ^ ^ ^ ^ ^
 
-read my_input # entering  Alice Bob  will fill the buffer, solution: enter "Alice Bob" instead
+read my_input # *terminal goes to next line* <text_to_input> ,entering  Alice Bob  will fill the buffer, solution: enter "Alice Bob" instead
 read -p "Enter your input:" my_input #display a message 
 
 #   ^ ^ ^ ^ ^ Console Output ^ ^ ^ ^ ^ 
